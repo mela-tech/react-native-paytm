8c4b9b42a8507543341fd1f35ca5bc5c65a07324
Anton Baldin <baldinant@gmail.com>
2018-12-19 01:16:26 +0200

    fix consecutive payments issue (iOS)

--- ios/RNPayTm.m
+++ ios/RNPayTm.m
@@ -15,10 +15,10 @@ RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
         [storage deleteCookie:cookie];
     }
     [[NSUserDefaults standardUserDefaults] synchronize];
-    
+
     NSMutableDictionary *orderDict = [NSMutableDictionary new];
     NSString* mode = details[@"mode"];
-    
+
     orderDict[@"MID"] = details[@"MID"];
     orderDict[@"CHANNEL_ID"] = details[@"CHANNEL_ID"];
     orderDict[@"INDUSTRY_TYPE_ID"] = details[@"INDUSTRY_TYPE_ID"];
@@ -30,11 +30,11 @@ RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
     orderDict[@"CUST_ID"] = details[@"CUST_ID"];
     orderDict[@"CHECKSUMHASH"] = details[@"CHECKSUMHASH"];
     orderDict[@"CALLBACK_URL"] = details[@"CALLBACK_URL"];
-    
+
     PGOrder *order = [PGOrder orderWithParams:orderDict];
-    
+
     PGTransactionViewController* txnController = [[PGTransactionViewController alloc] initTransactionForOrder:order];
-    
+
     if ([mode isEqualToString:@"Staging"]) {
         txnController.serverType = eServerTypeStaging;
         txnController.loggingEnabled = YES;
@@ -43,18 +43,16 @@ RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
         txnController.serverType = eServerTypeProduction;
     } else
         return;
-    
+
     txnController.merchant = [PGMerchantConfiguration defaultConfiguration];
     txnController.title = @"Paytm payment";
     txnController.delegate = self;
-    
+
     rootVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [rootVC presentViewController:txnController animated:YES completion:nil];
     });
-    
-    //    [rootVC.navigationController pushViewController:txnController animated:YES];
 }
 
 - (NSArray<NSString *> *)supportedEvents
@@ -73,7 +71,7 @@ RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
     //    [_statusTimer invalidate];
     NSString *msg = [NSString stringWithFormat:@"UnSuccessful"];
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Cancel transaction" message:msg preferredStyle:UIAlertControllerStyleAlert];
-    
+
     [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         [self sendEventWithName:@"PayTMResponse" body:@{@"status":@"Cancel", @"response":msg}];
         [rootVC dismissViewControllerAnimated:YES completion:nil];
@@ -81,9 +79,9 @@ RCT_EXPORT_METHOD(startPayment: (NSDictionary *)details)
     [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         [controller dismissViewControllerAnimated:YES completion:nil];
     }]];
-    
+
     [controller presentViewController:alertController animated:YES completion:nil];
-    
+
     //    [[[UIAlertView alloc] initWithTitle:@"Transaction Cancel" message:msg delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
     //    [self sendEventWithName:@"PayTMResponse" body:@{@"status":@"Cancel", @"response":msg}];
     //    [controller dismissViewControllerAnimated:YES completion:nil];
